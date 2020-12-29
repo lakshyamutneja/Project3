499ec2b90ae01564c83786981baf7c033b42e407
LAKSHYA MUTNEJA <mutnejalakshya98@gmail.com>
2020-12-29 15:29:00 +0530

    Update set_backend_ip_env.sh

--- set_backend_ip_env.sh
+++ set_backend_ip_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ip=$(curl -H "token:100754a2-c8e8-47f6-acc3-471691e1eb0e" --request GET https://api.memstash.io/values/backend )
+ip=$(curl -H "token: ae92a07f-b2f5-43bf-a767-e83093dd50cf" --request GET https://api.memstash.io/values/backend )
 echo 'testing'
 echo $ip
 export API_URL=$ip
